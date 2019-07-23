74326f06e682b9ff51702a9ad2609ce3690adeeb
Thomas <thomy.ripari@outlook.com>
2019-07-23 03:42:05 -0300

    Create High Order Component (withLink) to add active video selection links

--- src/components/containers/PlaylistItems.js
+++ src/components/containers/PlaylistItems.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 /* Import Components */
 import PlaylistItem from '../PlaylistItem';
+import withLink from '../hoc/withLink';
 
 /* Import Styled Components */
 import StyledPlaylistItems from '../../styles/StyledPlaylistItems';
 
+const PlayListItemWithLink = withLink(PlaylistItem);
+
 const PlaylistItems = ({videos, active}) => (
   <StyledPlaylistItems>
   	{videos.map(video => (
-  		<PlaylistItem
+  		<PlayListItemWithLink
   			key={video.id}
   			video={video}
   			active={video.id === active.id ? true : false}
