50d30e6b9f10b0597da4ad47f65144ce72b049e1
Thomas <thomy.ripari@outlook.com>
2019-07-23 00:03:23 -0300

    Add React VideoPlayer and styles

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
 
-const Video = () => (
-	<div>
-		VIDEO!!!
-	</div>
+/* Import Styled Components */
+import StyledVideo from '../styles/StyledVideo';
+import StyledVideoWrapper from '../styles/StyledVideoWrapper';
+
+const Video = ({active, autoplay, endCallback, progessCallback}) => (
+	<StyledVideo>
+		<StyledVideoWrapper>
+			<ReactPlayer
+				width="100%"
+				height="100%"
+				style={{position: "absolute", top: "0"}}
+				playing={autoplay}
+				controls={true}
+				url={active.video}
+				onEnded={endCallback}
+				onProgress={progessCallback}
+
+			 />
+		</StyledVideoWrapper>
+	</StyledVideo>
 )
 
 export default Video;
